ad1ff73bbfdcbd73d6085e3b467afc59b8761da0
gitboy03 <86159897+gitboy03@users.noreply.github.com>
2022-08-20 13:53:06 +0530

    Add files via upload
    Assignment-1
    Write a js program to convert temperature from celsius to farenheit and farenheit to celsius.

---
+++ Celsius_Farenheit.js
@@ -0,0 +1,8 @@
+var c = parseInt(prompt("Enter Celsius: "))
+var farenheit = (9*c/5)+32
+console.log(`Given Celsius: ${c}C 
+     Farenheit : ${farenheit}F`)
+var f = parseInt(prompt("Enter Farenheit: "))
+var celsius = ((f-32)/9)*5
+console.log(`Given Farenheit: ${f}F
+     Celsius : ${celsius}C`)

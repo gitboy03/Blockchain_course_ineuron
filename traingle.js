95aa40eb8809a2bc2c2f0164079a2c7c0d86319b
gitboy03 <86159897+gitboy03@users.noreply.github.com>
2022-09-01 19:29:01 +0530

    Add files via upload

---
+++ traingle.js
@@ -0,0 +1,13 @@
+var a ,b,c
+a = 1
+b = 3
+c = 1
+if (a == b & a == c){
+    console.log("Equilateral Triangle")
+}
+else if (a==b | b==c | a==c){
+    console.log("Isosceles Triangle")
+}
+else{
+    console.log("Scalene Triangle")
+}
